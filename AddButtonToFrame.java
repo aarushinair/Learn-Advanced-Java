e8e9a76d6eb375f0eea198ead9390502ed4f6128
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 22:21:56 +0530

    Create AddButtonToFrame.java

---
+++ AddButtonToFrame.java
@@ -0,0 +1,41 @@
+package com.awtSwing.example;
+
+import java.awt.FlowLayout;
+import javax.swing.*;
+
+public class AddButtonToFrame
+{
+  JFrame frame;
+  JButton button;
+ 
+  AddButtonToFrame()
+  {
+ frame = new JFrame("Welcome to Java Frame");
+ button = new JButton("Click Me!");
+ frame.setSize(200,100);
+ frame.setLayout(new FlowLayout());
+ frame.add(button);
+ frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ frame.setVisible(true);
+  }
+  
+  public static void main(String[] args)
+ {
+ new AddButtonToFrame();
+ }
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
