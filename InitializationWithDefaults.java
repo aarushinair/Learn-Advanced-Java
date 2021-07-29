12be2c11d34a378e7b602343188ca329831d8f7d
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 22:33:31 +0530

    Create InitializationWithDefaults.java

---
+++ InitializationWithDefaults.java
@@ -0,0 +1,41 @@
+package com.CodeWithAarushi.advanced.construction;
+
+public class InitializationWithDefaults {
+    private boolean booleanMember;
+    private byte byteMember;
+    private short shortMember;
+    private int intMember;
+    private long longMember;
+    private char charMember;
+    private float floatMember;
+    private double doubleMember;
+    private Object referenceMember;
+
+    public InitializationWithDefaults() {     
+        System.out.println( "booleanMember = " + booleanMember );
+        System.out.println( "byteMember = " + byteMember );
+        System.out.println( "shortMember = " + shortMember );
+        System.out.println( "intMember = " + intMember );
+        System.out.println( "longMember = " + longMember );
+        System.out.println( "charMember = " + 
+            Character.codePointAt( new char[] { charMember }, 0  ) );
+        System.out.println( "floatMember = " + floatMember );
+        System.out.println( "doubleMember = " + doubleMember );
+        System.out.println( "referenceMember = " + referenceMember );
+    }
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
