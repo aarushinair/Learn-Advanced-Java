6800d39373e3d1ed1eb86a43d0280b93ff4321fd
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 22:22:39 +0530

    Create SimpleFrame.java

---
+++ SimpleFrame.java
@@ -0,0 +1,34 @@
+package com.awtSwing.example;
+
+import javax.swing.*;
+
+public class SimpleFrame
+{
+  JFrame frame;
+  SimpleFrame()
+  {
+    frame = new JFrame("Welcome to Simple Java frame");
+    frame.setSize(200,100);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+   }
+  public static void main(String[] args)
+  {
+ new SimpleFrame();
+  }
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
