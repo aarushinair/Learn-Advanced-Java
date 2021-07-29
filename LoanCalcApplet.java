7659a64d55f665cd4dfa94c352bed4caadc9cbc9
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 22:59:15 +0530

    Create LoanCalcApplet.java

---
+++ LoanCalcApplet.java
@@ -0,0 +1,145 @@
+
+     // A simple loan calculator applet.
+     import java.awt.*;
+     import java.awt.event.*;
+     import javax.swing.*;
+     import java.text.*;
+     /*
+       <applet code="RegPay" width=320 height=200>
+</applet> */
+     public class RegPay extends JApplet
+       implements ActionListener {
+       JTextField amount Text, paymentText, periodText, rateText;
+       JButton doIt;
+       double principal; // original principal
+       double intRate;   // interest rate
+       double numYears;  // length of loan in years
+       /* Number of payments per year. You could
+          allow this value to be set by the user. */
+       final int payPerYear = 12;
+       NumberFormat nf;
+       public void init() {
+         try {
+           SwingUtilities.invokeAndWait(new Runnable () {
+             public void run() {
+               makeGUI(); // initialize the GUI
+             }
+           });
+         } catch(Exception exc) {
+System.out.println("Can't create because of "+ exc);
+} }
+// Set up and initialize the GUI.
+private void makeGUI() {
+  // Use a grid bag layout.
+  GridBagLayout gbag = new GridBagLayout();
+  GridBagConstraints gbc = new GridBagConstraints();
+  setLayout(gbag);
+  JLabel heading = new
+         JLabel("Compute Monthly Loan Payments");
+  JLabel amountLab = new JLabel("Principal ");
+  JLabel periodLab = new JLabel("Years ");
+  JLabel rateLab = new JLabel("Interest Rate ");
+  JLabel paymentLab = new JLabel("Monthly Payments ");
+  amountText = new JTextField(10);
+  periodText = new JTextField(10);
+  paymentText = new JTextField(10);
+  rateText = new JTextField(10);
+  // Payment field for display only.
+  paymentText.setEditable(false);
+  doIt = new JButton("Compute");
+  // Define the grid bag.
+  gbc.weighty = 1.0; // use a row weight of 1
+  gbc.gridwidth = GridBagConstraints.REMAINDER;
+  gbc.anchor = GridBagConstraints.NORTH;
+  gbag.setConstraints(heading, gbc);
+  // Anchor most components to the right.
+  gbc.anchor = GridBagConstraints.EAST;
+  gbc.gridwidth = GridBagConstraints.RELATIVE;
+  gbag.setConstraints(amountLab, gbc);
+  gbc.gridwidth = GridBagConstraints.REMAINDER;
+  gbag.setConstraints(amountText, gbc);
+  gbc.gridwidth = GridBagConstraints.RELATIVE;
+  gbag.setConstraints(periodLab, gbc);
+  gbc.gridwidth = GridBagConstraints.REMAINDER;
+  gbag.setConstraints(periodText, gbc);
+  gbc.gridwidth = GridBagConstraints.RELATIVE;
+  gbag.setConstraints(rateLab, gbc);
+  gbc.gridwidth = GridBagConstraints.REMAINDER;
+  gbag.setConstraints(rateText, gbc);
+  gbc.gridwidth = GridBagConstraints.RELATIVE;
+  gbag.setConstraints(paymentLab, gbc);
+  gbc.gridwidth = GridBagConstraints.REMAINDER;
+gbag.setConstraints(paymentText, gbc);
+gbc.anchor = GridBagConstraints.CENTER;
+gbag.setConstraints(doIt, gbc);
+// Add all the components.
+add(heading);
+add(amountLab);
+add(amountText);
+add(periodLab);
+add(periodText);
+add(rateLab);
+add(rateText);
+add(paymentLab);
+add(paymentText);
+add(doIt);
+// Register to receive action events.
+amountText.addActionListener(this);
+periodText.addActionListener(this);
+rateText.addActionListener(this);
+doIt.addActionListener(this);
+// Create a number format.
+nf = NumberFormat.getInstance();
+nf.setMinimumFractionDigits(2);
+nf.setMaximumFractionDigits(2);
+}
+/* User pressed Enter on a text field or
+   pressed Compute. Display the result if all
+   fields are completed. */
+public void actionPerformed(ActionEvent ae) {
+  double result = 0.0;
+  String amountStr = amountText.getText();
+  String periodStr = periodText.getText();
+  String rateStr = rateText.getText();
+  try {
+    if(amountStr.length() != 0 &&
+       periodStr.length() != 0 &&
+       rateStr.length() != 0) {
+      principal = Double.parseDouble(amountStr);
+      numYears = Double.parseDouble(periodStr);
+      intRate = Double.parseDouble(rateStr) / 100;
+      result = compute();
+      paymentText.setText(nf.format(result));
+    }
+showStatus(""); // erase any previous error message
+    } catch (NumberFormatException exc) {
+      showStatus("Invalid Data");
+      paymentText.setText("");
+} }
+  // Compute the loan payment.
+  double compute() {
+    double numer;
+    double denom;
+    double b, e;
+    numer = intRate * principal / payPerYear;
+    e = -(payPerYear * numYears);
+    b = (intRate / payPerYear) + 1.0;
+    denom = 1.0 - Math.pow(b, e);
+    return numer / denom;
+  }
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
