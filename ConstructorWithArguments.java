62380492d3b80e814e87b3abaa8fac00e39cb3fb
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-07-29 22:32:00 +0530

    Create ConstructorWithArguments.java

---
+++ ConstructorWithArguments.java
@@ -0,0 +1,26 @@
+package com.CodeWithAarushi.advanced.construction;
+
+public class ConstructorWithArguments {
+    public ConstructorWithArguments(final String arg1,final String arg2) {
+        // Constructor body here
+    }
+    
+    public ConstructorWithArguments(final String arg1) {
+        this(arg1, null);
+    }
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
