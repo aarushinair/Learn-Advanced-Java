699c681072451dd23ae5ee217b9b07d8f24cf567
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-01-31 14:48:02 +0530

    Update LoanCalcApplet.java

--- LoanCalcApplet.java
+++ LoanCalcApplet.java
@@ -134,6 +134,8 @@ showStatus(""); // erase any previous error message
 
 
 
+
+
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
