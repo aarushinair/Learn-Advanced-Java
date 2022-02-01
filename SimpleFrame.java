34578c3f278a138e1f1e72863f3be25f692fe1eb
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-02-01 17:59:28 +0530

    Update SimpleFrame.java

--- SimpleFrame.java
+++ SimpleFrame.java
@@ -22,8 +22,6 @@ public class SimpleFrame
 
 
 
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
