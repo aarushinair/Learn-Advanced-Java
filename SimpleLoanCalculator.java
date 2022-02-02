44240b1a6793a9de236fe342ebe1b7425e95e95f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-02-02 09:23:47 +0530

    Update SimpleLoanCalculator.java

--- SimpleLoanCalculator.java
+++ SimpleLoanCalculator.java
@@ -85,10 +85,6 @@ double compute() {
 
 
 
-
-
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
