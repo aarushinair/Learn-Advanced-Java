008df73731689433ffaed6d1389d939c12e2f8a4
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-10-18 08:03:56 +0530

    Update LoanBalance.java

--- LoanBalance.java
+++ LoanBalance.java
@@ -146,7 +146,7 @@ return bal; }
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -154,3 +154,4 @@ return bal; }
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
